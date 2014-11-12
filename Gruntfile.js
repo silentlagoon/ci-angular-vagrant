a1a3db7897164a76dc4df463a0248b639a64b852
Dmitry Kurilenko <dmitry@intspirit.com>
2014-11-12 13:25:28 +0300

    grunt test task added

--- Gruntfile.js
+++ Gruntfile.js
@@ -19,6 +19,9 @@ module.exports = function(grunt) {
 	});
 	grunt.loadNpmTasks('grunt-karma');
 	grunt.loadNpmTasks('grunt-phpunit');
+	grunt.registerTask('test', [
+        'phpunit', 'karma'
+    ]);
 	grunt.registerTask('default', [
         'phpunit', 'karma'
     ]);
